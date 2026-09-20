37ead4f821ad5904630f96b68982156bbb433db8
Evelyn-13 <evelyntvg13@gmail.com>
2026-09-19 23:51:34 -0500

    Se agregaron las vistas de DetalleCasoScreen.kt, CrearCasoScreen.kt, EditarCasoScreen.kt Y se agregó PantallaMain.kt que será la pantalla principal pero no estan completadas

--- app/src/main/java/com/udistrital/detectiveapp/MainActivity.kt
+++ app/src/main/java/com/udistrital/detectiveapp/MainActivity.kt
@@ -6,12 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Column
 import androidx.compose.ui.tooling.preview.Preview
+import com.udistrital.detectiveapp.ui.case.DetalleCaso
 import com.udistrital.detectiveapp.ui.theme.DetectiveAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +27,46 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DetectiveAppTheme {
+//trasladarlo a la pagina de inicio cuando se cree
+                var pantallaActual by remember { mutableStateOf("inicio") }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    when (pantallaActual) {
+                        "inicio" ->
+                            Greeting(
+                                name = "Android",
+                                modifier = Modifier.padding(innerPadding),
+                                onBotonClick = { pantallaActual = "detalle" } // <- la acción
+                            )
+
+                        "detalle" ->
+                            DetalleCaso(
+                                onBotonClick = { pantallaActual = "inicio" }, // regresar, opcional
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier, onBotonClick: () -> Unit) {
+                Column (modifier = modifier) {
+                Text(text = "Hello $name!")
+                Button(onClick = onBotonClick) {
+                    Text("Ver detalle")
+                }
+            }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DetectiveAppTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        DetectiveAppTheme {
+            Greeting(
+                "inicio",
+                onBotonClick = {})
+        }
     }
 }
